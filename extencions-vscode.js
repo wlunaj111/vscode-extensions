5efc2706328f0262fdaf27123afa80f2005615e9
Wilber <wlunaj111@gmail.com>
2022-07-24 18:37:17 -0400

    .
    .

--- extencions-vscode.js
+++ extencions-vscode.js
@@ -1,1 +1,63 @@
-holaaa
+// para configurar settings 
+//f1   buscar settings json 
+    //configurando el json 
+    {
+        "editor.tabSize": 2, //  Para que se mueva dos espacios con la tecla Tab
+        "editor.wordWrap": "on",  // Para mostrar todo en pantalla cuando el codigo es muy extenso 
+        //emmet...  para autocompletar en react html desntro de js 
+        "emmet.includeLanguages":{ 
+            "javascript":"javascriptreact"
+        } 
+    }
+
+// Extenciones de vscode 
+material icon theme para el estilo de las carpetas
+
+// Para la apariencia del editor dos opciones a escoger 
+material theme
+dobri next // cambia el color de las letras en la sintaxis 
+
+//instalacion de otro editor 
+Vim // Se pueden utilizar sus atajos entro de vscode
+
+// Para las variables de entorno 
+dotenv
+
+//Para git 
+gitlens
+
+//Para trabajar con flutter 
+Flutter
+
+// Para autocompletado en algun projecto .net 
+c#
+
+//Para GraphQL
+graphql 
+
+//Para python 
+python 
+
+//Para Go 
+go 
+
+//Para Rust
+rust 
+
+//Extenciones de javascript 
+EsLint 
+prettier //para formtear codigo de js
+Angular Essentials
+Vetur //para Vue
+Deno //Para deno 
+
+// Para frontend 
+live server // Para montar un servidor de pprueba 
+es7 react // Para react te pone snippets para escoger autocompletado
+tailwindcs
+docker 
+
+//Para diferenciar las llaves poniendo un color para identificarlas dentro del mismo editor en settings buscar bracket color y habilitar
+// Esto automaticamente le da un color diferente a cada llaves, corchete etc.
+
+
